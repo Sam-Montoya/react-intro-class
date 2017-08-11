4a05ec3383bf187bd0093ecf5aa41d85ff916ec8
Samuel <samuel.montoya1073@gmail.com>
2017-08-11 15:54:42 -0600

    Finished the Assignment.

--- app/src/components/Calculator.js
+++ app/src/components/Calculator.js
@@ -10,7 +10,7 @@ class Calculator extends Component {
             header: "This is a Calculator...",
             display: "0",
             operator: '',
-            temp: 0,
+            temp: '0',
             resetDisplay: false
         }
     }
@@ -29,15 +29,43 @@ class Calculator extends Component {
         else
             display = this.state.display + num;
 
-        if(this.state.display.length < 13){
+        if(display < 13){
             this.setState({
                 display: display
             });
         }
     }
 
+    setOperator(operator){
+        if(this.state.operator === ''){
+            this.setState({
+                operator: operator,
+                temp: parseInt(this.state.display),
+                display: '0'
+            });
+        }
+    }
+
+    calculate(){
+        if(this.state.operator === ''){ return;  }
+
+        var result;
+        if(this.state.operator === '*')
+            result = this.state.temp * parseInt(this.state.display);
+        else if(this.state.operator === '/')
+            result = this.state.temp / parseInt(this.state.display);
+        else if(this.state.operator === '-')
+            result = this.state.temp - parseInt(this.state.display);
+        else if(this.state.operator === '+')
+            result = this.state.temp + parseInt(this.state.display);
+
+        this.setState({
+            display: result
+        });
+    }
+
     onPressClear(){
-        this.setState({display: ""});
+        this.setState({display: "", temp: '0', operator: null});
     }
 
     render() {
@@ -54,22 +82,22 @@ class Calculator extends Component {
 
                     <div className="btn clear"  onClick =   {( click )  =>  this.onPressClear()}></div>
 
-                    <div className="btn zero"   onClick =   {( click )  => this.onPressNum('0')}></div>
-                    <div className="btn one"    onClick =   {( click )  => this.onPressNum('1')}></div>
-                    <div className="btn two"    onClick =   {( click )  => this.onPressNum('2')}></div>
-                    <div className="btn three"  onClick =   {( click )  => this.onPressNum('3')}></div>
-                    <div className="btn four"   onClick =   {( click )  => this.onPressNum('4')}></div>
-                    <div className="btn five"   onClick =   {( click )  => this.onPressNum('5')}></div>
-                    <div className="btn six"    onClick =   {( click )  => this.onPressNum('6')}></div>
-                    <div className="btn seven"  onClick =   {( click )  => this.onPressNum('7')}></div>
-                    <div className="btn eight"  onClick =   {( click )  => this.onPressNum('8')}></div>
-                    <div className="btn nine"   onClick =   {( click )  => this.onPressNum('9')}></div>
+                    <div className="btn zero"   onClick =   {( click )  =>  this.onPressNum('0')}></div>
+                    <div className="btn one"    onClick =   {( click )  =>  this.onPressNum('1')}></div>
+                    <div className="btn two"    onClick =   {( click )  =>  this.onPressNum('2')}></div>
+                    <div className="btn three"  onClick =   {( click )  =>  this.onPressNum('3')}></div>
+                    <div className="btn four"   onClick =   {( click )  =>  this.onPressNum('4')}></div>
+                    <div className="btn five"   onClick =   {( click )  =>  this.onPressNum('5')}></div>
+                    <div className="btn six"    onClick =   {( click )  =>  this.onPressNum('6')}></div>
+                    <div className="btn seven"  onClick =   {( click )  =>  this.onPressNum('7')}></div>
+                    <div className="btn eight"  onClick =   {( click )  =>  this.onPressNum('8')}></div>
+                    <div className="btn nine"   onClick =   {( click )  =>  this.onPressNum('9')}></div>
 
-                    <div className="btn equal"></div>
-                    <div className="btn multiply"></div>
-                    <div className="btn divide"></div>
-                    <div className="btn subtract"></div>
-                    <div className="btn add"></div>
+                    <div className="btn equal"      onClick =   {( click )  =>  this.calculate()}></div>
+                    <div className="btn multiply"   onClick =   {( click )  =>  this.setOperator('*')}></div>
+                    <div className="btn divide"     onClick =   {( click )  =>  this.setOperator('/')}></div>
+                    <div className="btn subtract"   onClick =   {( click )  =>  this.setOperator('-')}></div>
+                    <div className="btn add"        onClick =   {( click )  =>  this.setOperator('+')}></div>
                 </div>
             </div>
         )
